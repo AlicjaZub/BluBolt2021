d79829348ce497e6f3989b4e201b29d69f4bb6e9
Alicja <puranut@hotmail.com>
2021-08-24 19:00:01 +0100

    module 2 done

--- moduleTwoJSAndPrimatives/index.js
+++ moduleTwoJSAndPrimatives/index.js
@@ -1,6 +1,4 @@
-
-
-// baseData 
+// baseData
 const baseData = [
   {
     id: 1,
@@ -28,22 +26,100 @@ const baseData = [
   },
 ]
 
-// Add on Data
-
 const addOnData = {
   id: 20,
   value: "I am addon data"
 }
 
-console.log('JS Loaded and ready to log!')
-console.log({baseData, addOnData})
-
-
 /**
  * Your Event listners and Loggers should be added below
  */
 
+document.querySelector('#button1').addEventListener('click', () => {
+  // console.log(baseData)
+  baseData.forEach(data => console.log(data['id'], data['value']))
+})
 
+let reversedBaseData = [...baseData].reverse() //inside to reverse continouslly
+// let reversedBaseData = baseData.slice().reverse();
+
+document.querySelector('#button2').addEventListener('click', () => {
+  // console.log(reversedBaseData)
+  reversedBaseData.forEach(data => console.log(data['id'], data['value']))
+})
+
+let withoutFirstItemBaseData = [...baseData];
+withoutFirstItemBaseData.shift();
+
+document.querySelector('#button3').addEventListener('click', () => {
+  withoutFirstItemBaseData.forEach(data => console.log(data['id'], data['value']))
+})
+
+document.querySelector('#button4').addEventListener('click', () => {
+  baseData.forEach(data => console.log(data['id'], data['value']))
+})
+
+let addObjectBaseData = [...baseData]
+addObjectBaseData.unshift(addOnData)
+
+document.querySelector('#button5').addEventListener('click', () => {
+  addObjectBaseData.forEach(data => console.log(data['id'], data['value']))
+})
+
+let replaceObjectBaseData = [...baseData]
+replaceObjectBaseData[2] = addOnData
+
+document.querySelector('#button6').addEventListener('click', () => {
+  replaceObjectBaseData.forEach(data => console.log(data['id'], data['value']))
+})
+
+/**
+ * String stuff
+ */
+
+const string = document.querySelector('#stringText').textContent
+
+document.querySelector('#button7').addEventListener('click', () => {
+  console.log(string.match(/fox/g).length)
+})
+
+let arrayOfWords = string.split(" ")
+
+function reverseWords(arrayOfWords)
+{
+  let reversedWordsArray = []
+  arrayOfWords.forEach(word => {
+    reversedWordsArray.push(reverseWord(word))
+  })
+  return reversedWordsArray
+}
+
+function reverseWord(word)
+{
+  if (word == "") {
+    return ""
+  } else
+    return reverseWord(word.substr(1)) + word.charAt(0);
+}
+
+document.querySelector('#button8').addEventListener('click', () => {
+  let reversedString = reverseWords(arrayOfWords).join(" ")
+  console.log(reversedString)
+})
+
+document.querySelector('#button9').addEventListener('click', () => {
+  console.log(string.replace(/brown/g, "sandy"))
+})
+
+// window.localStorage.clear()
+if (window.localStorage.getItem('value') !== null) {
+  document.querySelector('#addToLocalStorage').value +=  window.localStorage.getItem('value')
+}
+
+document.querySelector('#addToLocalStorage').addEventListener('blur', () => {
+  window.localStorage.setItem('value', document.querySelector('#addToLocalStorage').value )
+  console.log(localStorage)
+})
 
 
 
